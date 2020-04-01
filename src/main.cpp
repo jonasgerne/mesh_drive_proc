176c3927ac82a46e1b1f3404a837b2cb27f776b3
Jonas <jonas.gerstner@tum.de>
2020-04-01 20:34:47 +0200

    optional callback

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,7 @@ class MeshAnalyzer{
 public:
     MeshAnalyzer(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private);
     void mesh_cb(const pcl_msgs::PolygonMeshPtr &input_mesh);
+    void mesh_cb_neighborhood(const pcl_msgs::PolygonMeshPtr &input_mesh);
     bool savePLY(std_srvs::Empty::Request&, std_srvs::Empty::Response&);
 
 private:
@@ -42,11 +43,19 @@ private:
     pcl::PointCloud<pcl::PointXYZRGBNormal>::Ptr last_pcl_traversable_;
 
     std::string path_ply_;
+
+    bool check_local_neighborhood_;
 };
 
 MeshAnalyzer::MeshAnalyzer(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private) :
     nh_(nh), nh_private_(nh_private) {
-    mesh_sub_ = nh_.subscribe("input", 1, &MeshAnalyzer::mesh_cb, this);
+
+    nh_private_.param("local_neighborhood", check_local_neighborhood_, false);
+
+    if (check_local_neighborhood_)
+        mesh_sub_ = nh_.subscribe("input", 1, &MeshAnalyzer::mesh_cb_neighborhood, this);
+    else
+        mesh_sub_ = nh_.subscribe("input", 1, &MeshAnalyzer::mesh_cb, this);
 
     ROS_INFO("Subscribed to %s", mesh_sub_.getTopic().c_str());
 
@@ -65,19 +74,57 @@ MeshAnalyzer::MeshAnalyzer(const ros::NodeHandle &nh, const ros::NodeHandle &nh_
 void MeshAnalyzer::mesh_cb(const pcl_msgs::PolygonMeshPtr &input_mesh){
     ROS_INFO("Received mesh_pcl message.");
 
-    half_edge_mesh_.clear();
-
-    bool has_normals = false;
-    for (auto &field : input_mesh->cloud.fields) {
-        if (field.name == "normal_x")
-            has_normals = true;
-    }
-    ROS_INFO_COND(!has_normals, "Incoming point cloud message has no vertex normals.");
     auto *mesh = new pcl::PolygonMesh;
     pcl::PolygonMeshConstPtr meshPtr(mesh);
     pcl_conversions::toPCL(*input_mesh, *mesh);
 
-    
+    pcl::PointCloud<pcl::PointXYZ>::Ptr vertices( new pcl::PointCloud<pcl::PointXYZ> );
+    pcl::fromPCLPointCloud2(meshPtr->cloud, *vertices);
+
+    last_pcl_traversable_.reset(new pcl::PointCloud<pcl::PointXYZRGBNormal>);
+
+    const auto& faces = meshPtr->polygons;
+
+    for (const auto& face : faces) {
+
+        Eigen::Matrix3f mat;
+
+        if (face.vertices.size() > 3)
+            continue;
+
+        for (int i = 0; i < 3; ++i){
+            mat.col(i) = vertices->operator[](face.vertices[i]).getVector3fMap();
+        }
+
+        Eigen::Vector3f normal = (mat.col(0) - mat.col(1)).cross(mat.col(0) - mat.col(2));
+        normal.normalize();
+
+        Eigen::Vector3f center = mat.rowwise().sum() / 3.0;
+
+        pcl::PointXYZRGBNormal p_temp;
+        p_temp.x = center.x();
+        p_temp.y = center.y();
+        p_temp.z = center.z();
+        p_temp.normal_x = normal.x();
+        p_temp.normal_y = normal.y();
+        p_temp.normal_z = normal.z();
+
+        float surface_angle = normal.dot(Eigen::Vector3f::UnitZ());
+
+        if (surface_angle < cos_max_surface_angle_)
+            p_temp.r = 255;
+        else
+            p_temp.g = 255;
+
+        last_pcl_traversable_->push_back(p_temp);
+
+    }
+
+    last_pcl_traversable_->header.frame_id = "world";
+    pcl_pub_.publish(last_pcl_traversable_);
+    ROS_INFO("Finished processing pointcloud.");
+
+    /*
     int not_added = pcl::geometry::toHalfEdgeMesh(*meshPtr, half_edge_mesh_);
     ROS_INFO("%d faces could not be added.", not_added);
     ROS_INFO("Size Edges: %ld, size faces: %ld", half_edge_mesh_.sizeEdges(), half_edge_mesh_.sizeFaces());
@@ -126,9 +173,33 @@ void MeshAnalyzer::mesh_cb(const pcl_msgs::PolygonMeshPtr &input_mesh){
     last_pcl_traversable_->header.frame_id = "world";
     pcl_pub_.publish(last_pcl_traversable_);
     ROS_INFO("Finished processing pointcloud.");
+    */
+}
+
+void MeshAnalyzer::mesh_cb_neighborhood(const pcl_msgs::PolygonMeshPtr &input_mesh){
+    ROS_INFO("Received mesh_pcl message.");
+
+    half_edge_mesh_.clear();
+
+    bool has_normals = false;
+    for (auto &field : input_mesh->cloud.fields) {
+        if (field.name == "normal_x")
+            has_normals = true;
+    }
+    ROS_INFO_COND(!has_normals, "Incoming point cloud message has no vertex normals.");
+    auto *mesh = new pcl::PolygonMesh;
+    pcl::PolygonMeshConstPtr meshPtr(mesh);
+    pcl_conversions::toPCL(*input_mesh, *mesh);
+
+
+    int not_added = pcl::geometry::toHalfEdgeMesh(*meshPtr, half_edge_mesh_);
+    ROS_INFO("%d faces could not be added.", not_added);
+    ROS_INFO("Size Edges: %ld, size faces: %ld", half_edge_mesh_.sizeEdges(), half_edge_mesh_.sizeFaces());
+
+    half_edge_mesh_.cleanUp();
 
     // 1. Iterate over all faces and calculate face normal
-    /*ROS_INFO("Iterating over faces.");
+    ROS_INFO("Iterating over faces.");
     const pcl::PointCloud<MyLabeledPointType> &vertex_cloud = half_edge_mesh_.getVertexDataCloud();
 
     for (unsigned int i = 0; i < half_edge_mesh_.sizeFaces(); ++i) {
@@ -186,7 +257,7 @@ void MeshAnalyzer::mesh_cb(const pcl_msgs::PolygonMeshPtr &input_mesh){
 
     last_pcl_traversable_->header.frame_id = "world";
     pcl_pub_.publish(last_pcl_traversable_);
-    ROS_INFO("Finished processing pointcloud.");*/
+    ROS_INFO("Finished processing pointcloud.");
 }
 
 bool MeshAnalyzer::savePLY(std_srvs::Empty::Request& /*request*/,
